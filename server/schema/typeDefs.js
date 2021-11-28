bf16fc9b66e74701b2790de15b2a7e6cbd6f0281
Nick Zabkar <nzabkar51@gmail.com>
2021-11-27 22:45:57 -0500

    Added types and returned in mutations

--- server/schema/typeDefs.js
+++ server/schema/typeDefs.js
@@ -15,6 +15,23 @@ const schema = buildSchema(`
         id: String!
         username: String!
         email: String!
+        savedBooks: [
+            Book
+        ]
+    }
+    type Book {
+        authors: [
+            String!
+        ]
+        description: String!
+        bookId: String!
+        image: String
+        link: String
+        title: String!
+    }
+    type GetSingleUserResponse {
+        statusCode: Int
+        user: User
     }
     type CreateUserResponse {
         token: String
@@ -23,12 +40,25 @@ const schema = buildSchema(`
     type Query {
         getSingleUser(id: String, userName: String):User
     }
+    type SaveBookResponse {
+        user: User
+        statusCode: Int!
+    }
+    type DeleteBookResponse {
+        user: User
+        statusCode: Int!
+    }
+    type LoginResponse {
+        user: User
+        statusCode: Int!
+        token: String
+    }
     type Mutation {
-        createUser(userName: String, email: String, password: String):User
-        saveBook(book: BookInput!):
-        deleteBook(bookId: String!)
-        login(username: String, email: String, password: String!)
+        createUser(userName: String, email: String, password: String): CreateUserResponse!
+        saveBook(book: BookInput!): SaveBookResponse!
+        deleteBook(bookId: String!): DeleteBookResponse!
+        login(username: String, email: String, password: String!): LoginResponse!
     }
 `);
 
-module.exports = typeDefs;
+module.exports = schema;
