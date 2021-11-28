83e6c783b13044971e209d471070d056e76a9014
Nick Zabkar <nzabkar51@gmail.com>
2021-11-27 22:49:56 -0500

    removed "res", converted to return object instead

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -5,19 +5,25 @@ const { signToken } = require('../utils/auth');
 
 module.exports = {
   // get a single user by either their id or their username
-  async getSingleUser({ user = null, params }, res) {
+  async getSingleUser({ user = null, params }) {
     const foundUser = await User.findOne({
       $or: [{ _id: user ? user._id : params.id }, { username: params.username }],
     });
 
     if (!foundUser) {
-      return res.status(400).json({ message: 'Cannot find a user with this id!' });
+      return {
+        statusCode: 400,
+        message: 'Cannot find a user with this id!'
+      }
     }
 
-    res.json(foundUser);
+    return {
+      statusCode: 200,
+      user: foundUser
+    }
   },
   // create a user, sign a token, and send it back (to client/src/components/SignUpForm.js)
-  async createUser({ body }, res) {
+  async createUser({ body }, ) {
     const user = await User.create(body);
 
     if (!user) {
@@ -28,7 +34,7 @@ module.exports = {
   },
   // login a user, sign a token, and send it back (to client/src/components/LoginForm.js)
   // {body} is destructured req.body
-  async login({ body }, res) {
+  async login({ body }, ) {
     const user = await User.findOne({ $or: [{ username: body.username }, { email: body.email }] });
     if (!user) {
       return res.status(400).json({ message: "Can't find this user" });
@@ -44,7 +50,7 @@ module.exports = {
   },
   // save a book to a user's `savedBooks` field by adding it to the set (to prevent duplicates)
   // user comes from `req.user` created in the auth middleware function
-  async saveBook({ user, body }, res) {
+  async saveBook({ user, body }, ) {
     console.log(user);
     try {
       const updatedUser = await User.findOneAndUpdate(
@@ -59,7 +65,7 @@ module.exports = {
     }
   },
   // remove a book from `savedBooks`
-  async deleteBook({ user, params }, res) {
+  async deleteBook({ user, params }, ) {
     const updatedUser = await User.findOneAndUpdate(
       { _id: user._id },
       { $pull: { savedBooks: { bookId: params.bookId } } },
